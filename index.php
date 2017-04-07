babf8d22fe58d4cacd7653ebc2a5d636aa456d17
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2017-04-07 13:56:03 +0000

    more init stuff

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <title>Push To MySQL</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
+      <script src="js/script.js"></script>
     </body>
 </html>
