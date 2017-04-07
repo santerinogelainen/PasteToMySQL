884a33c96ed206003390ee6f28fa2a149840d0aa
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2017-04-07 13:45:52 +0000

    init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Paste To MySQL
+
+Paste CSV / Other separated data to the browser and add the data to a MySQL table. Useful for pasting data from MS Access.
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Push To MySQL</title>
+    </head>
+    <body>
+    </body>
+</html>
